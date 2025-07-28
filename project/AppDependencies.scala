e512e10837ef63c9a41d53d3bade5ac8033284f1
Lee Powell <lee.powell@digital.hmrc.gov.uk>
2025-07-28 16:36:27 +0100

    VEOSS-2117 | Fix Bobby rule enforcement error

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.18.0"
+  private val hmrcMongoVersion = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"  % bootstrapVersion,
